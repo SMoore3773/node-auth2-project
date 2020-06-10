214fadbe59cc876ee03b22a2d3b39a13510fe10b
SMoore3773 <Smooresculpture@gmail.com>
2020-06-09 21:43:36 -0400

    proper error messages

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -11,7 +11,7 @@ module.exports = (req,res,next) =>{
     if(token){
         jwt.verify(token, secrets.jwtSecret, (err, decodedToken)=>{
             if(err){
-                res.status(401).json({message:"error",err})
+                res.status(401).json({message:"You shall not pass!", reason:err.message})
             }else{
         req.decodedJwt = decodedToken;
         console.log(decodedToken);
